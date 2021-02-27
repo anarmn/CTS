610dbfefc44477dfb6c199c60b5676f6cb8b66b9
anarmn <roman.ana980@gmail.com>
2021-02-27 13:21:03 +0200

    added zoo related classes

--- seminar1/src/program/Main.java
+++ seminar1/src/program/Main.java
@@ -1,5 +1,13 @@
 package program;
 
 public class Main {
+  public static void main(String[] args){
+      Zoo zoo = new Zoo();
+      Giraffe g1 = new Giraffe("Luminita");
+      Giraffe g2 = new Giraffe("Georgeta");
 
+      zoo.addAnimal(g1);
+      zoo.addAnimal(g2);
+      zoo.feedAllAnimals();
+  }
 }

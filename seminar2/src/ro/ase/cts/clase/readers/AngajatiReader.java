1b96523f37654d77bfe1b29a116c36212a66a5be
anarmn <roman.ana980@gmail.com>
2021-03-13 12:26:20 +0200

    changed interface into abstract class

--- seminar2/src/ro/ase/cts/clase/readers/AngajatiReader.java
+++ seminar2/src/ro/ase/cts/clase/readers/AngajatiReader.java
@@ -5,33 +5,36 @@ import ro.ase.cts.clase.Aplicant;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.CharBuffer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class AngajatiReader implements Readable{
-    public List<Aplicant> readAplicanti(String file) throws FileNotFoundException {
-        Scanner input2 = new Scanner(new File(file));
+public class AngajatiReader extends Reader{
+
+    public AngajatiReader(String numeFisier) {
+        super(numeFisier);
+    }
+
+    public List<Aplicant> readAplicanti() throws FileNotFoundException {
+        Scanner input2 = new Scanner(new File(super.numeFisier));
         input2.useDelimiter(",");
         List<Aplicant> angajati = new ArrayList<Aplicant>();
 
         while (input2.hasNext()) {
-            String nume = input2.next();
-            String prenume = input2.next();
-            int varsta = input2.nextInt();
-            int punctaj = input2.nextInt();
-            int nr = input2.nextInt();
-            String[] vect = new String[5];
-            for (int i = 0; i < nr; i++)
-                vect[i] = input2.next();
+           Angajat angajat = new Angajat();
+           super.readAplicant(input2, angajat);
             int salariu = input2.nextInt();
             String ocupatie = input2.next();
-            Angajat a = new Angajat(nume, prenume, varsta, punctaj, nr, vect, salariu, ocupatie);
-            angajati.add(a);
+            angajat.setSalariu(salariu);
+            angajat.setOcupatie(ocupatie);
+            angajati.add(angajat);
         }
         input2.close();
         return angajati;
     }
+
 
 
 }
--- seminar2/src/ro/ase/cts/program/Program.java
+++ seminar2/src/ro/ase/cts/program/Program.java
@@ -4,7 +4,8 @@ import ro.ase.cts.clase.Angajat;
 import ro.ase.cts.clase.Aplicant;
 import ro.ase.cts.clase.readers.AngajatiReader;
 import ro.ase.cts.clase.readers.EleviReader;
-import ro.ase.cts.clase.readers.Readable;
+
+import ro.ase.cts.clase.readers.Reader;
 import ro.ase.cts.clase.readers.StudentiReader;
 
 import java.io.FileNotFoundException;
@@ -12,14 +13,14 @@ import java.util.List;
 
 public class Program {
 
-	public static List<Aplicant> readAplicants(String numeFisier, Readable reader) throws FileNotFoundException {
-		return reader.readAplicanti(numeFisier);
+	public static List<Aplicant> readAplicants(Reader reader) throws FileNotFoundException {
+		return reader.readAplicanti();
 	}
 
 	public static void main(String[] args) {
 		List<Aplicant> listaAplicanti;
 		try {
-			listaAplicanti = readAplicants("seminar2/elevi.txt", new EleviReader());
+			listaAplicanti = readAplicants(new EleviReader("seminar2/elevi.txt"));
 			for(Aplicant aplicant:listaAplicanti)
 				System.out.println(aplicant.toString());
 		} catch (FileNotFoundException e) {

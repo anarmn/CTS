8c5be069e076b64a5673cb2a6cc02b7fa1762f7b
anarmn <roman.ana980@gmail.com>
2021-04-10 13:04:52 +0300

    decorator

---
+++ seminar 7 decorator/src/ro/ase/cts/clase/CardBancar.java
@@ -0,0 +1,7 @@
+package ro.ase.cts.clase;
+
+public interface CardBancar {
+    public void platestePOS();
+    public void platesteOnline();
+
+}
---
+++ seminar 7 decorator/src/ro/ase/cts/clase/DecoratorAbstract.java
@@ -0,0 +1,25 @@
+package ro.ase.cts.clase;
+
+public abstract class DecoratorAbstract implements CardBancar{
+    private CardBancar card;
+
+    public DecoratorAbstract(CardBancar card) {
+        this.card = card;
+    }
+
+    public abstract void platesteContactLess();
+
+    @Override
+    public void platestePOS() {
+        this.card.platestePOS();
+    }
+
+    @Override
+    public void platesteOnline() {
+        this.card.platesteOnline();
+    }
+
+    public CardBancar getCard() {
+        return card;
+    }
+}

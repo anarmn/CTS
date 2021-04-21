e51e4461d837b29478896f20de9301c1e694286c
anarmn <roman.ana980@gmail.com>
2021-04-21 07:52:19 +0300

    proxy

---
+++ seminar 9 proxy/src/ro/ase/cts/clase/ProxyManager.java
@@ -0,0 +1,19 @@
+package ro.ase.cts.clase;
+
+public class ProxyManager implements Rezervabil{
+    private ManagerRezervari managerRezervari;
+    private int nrMinimPersoane;
+
+    public ProxyManager(ManagerRezervari managerRezervari, int nrMinimPersoane) {
+        this.managerRezervari = managerRezervari;
+        this.nrMinimPersoane = nrMinimPersoane;
+    }
+
+    @Override
+    public void rezerva(int nrPersoane) {
+        if (nrPersoane>=this.nrMinimPersoane) managerRezervari.rezerva(nrPersoane);
+        else {
+            System.out.println("Rezervarea nu se poate efectua. Veniti fara rezervare la restaurant.");
+        }
+    }
+}
---
+++ seminar 9 proxy/src/ro/ase/cts/clase/Rezervabil.java
@@ -0,0 +1,6 @@
+package ro.ase.cts.clase;
+
+public interface Rezervabil {
+
+    public void rezerva(int nrPersoane);
+}
